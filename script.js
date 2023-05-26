95702208162cb5738369490956f04695c9a8d467
ponlawat-w <ponlawat@whereismytransport.com>
2023-05-26 16:07:35 +0700

    Updated

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ video.setAttribute('autoplay', '');
 video.setAttribute('muted', '');
 video.setAttribute('playsinline', '');
 
-navigator.mediaDevices.getUserMedia({ video: {
-  facingMode: { ideal: 'environment' }
-}, audio: false })
-  .then(stream => {
-    if ('srcObject' in video) {
-      video.srcObject = stream;
-    } else {
-      video.src = window.URL.createObjectURL(stream);
-    }
-    video.play();
-  }).catch(err => {
-    console.error(`Not available!!!!`, err);
-  });
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+
+const img = document.getElementById('img');
+
+let coords = { latitude: 0, longitude: 0 };
+
+const init = async() => {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: {
+    facingMode: { ideal: 'environment' }
+  }});
+  if ('srcObject' in video) {
+    video.srcObject = stream;
+  } else {
+    video.src = window.URL.createObjectURL(stream);
+  }
+  video.play();
+  const videoTrack = stream.getVideoTracks()[0];
+  canvas.width = videoTrack.getSettings().width;
+  canvas.height = videoTrack.getSettings().height;
+};
+init();
 
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(showPosition);
@@ -24,6 +33,13 @@ if (navigator.geolocation) {
 }
 
 function showPosition(position) {
-  const target = document.getElementById('location');
-  target.innerHTML = 'Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude;
+  coords = position.coords;
 }
+
+setInterval(() => {
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  const dataUrl = canvas.toDataURL('image/png');
+  img.setAttribute('src', dataUrl);
+
+  document.getElementById('description').innerHTML = `${new Date().toLocaleString()}　Location: ${Math.round(coords.latitude * 100000) / 100000},${Math.round(coords.longitude * 100000) / 100000}`
+}, 1000);
